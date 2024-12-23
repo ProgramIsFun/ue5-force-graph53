b460a2cae4b02d921301b78f7dd13e7d75319ac0
Your Name <you@example.com>
2024-12-23 18:13:02 +0800

    Revert "a"
    This reverts commit 48b2c1fd673a9a0f1144f9cc025115e6be9af5c6.

--- Plugins/SettingsWidgetConstructor/Source/SettingsWidgetConstructor/Private/Data/SettingFunction.cpp
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Yevhenii Selivanov
-
-#include "Data/SettingFunction.h"
-//---
-#include UE_INLINE_GENERATED_CPP_BY_NAME(SettingFunction)
-
-// Empty setting function data
-const FSettingFunctionPicker FSettingFunctionPicker::EmptySettingFunction = FSettingFunctionPicker();

6b2cded9dc8455efef745eb962a914d3f697badf
name71 <a1@a2.a3>
2026-03-12 20:44:25 -0700

    clean

--- Source/NBodySimulation/GraphSystem/NBodyUtils.h
+++ Source/NBodySimulation/GraphSystem/NBodyUtils.h
@@ -1,44 +1,13 @@
-﻿// Refactored from utillllllssss.h in commit cfe5a2e49166b9a584b865688086bef641dc2862
-#pragma once
-
-#include <string>
+﻿#pragma once
 
 #include "CoreMinimal.h"
 
-// #ifndef ENABLE_LOGGING
-// #define ENABLE_LOGGING 0
-// #endif
-//
-//
-//
-// #if ENABLE_LOGGING
-// #define ll(StringToLog, ...) lll2(StringToLog, ##__VA_ARGS__)
-// #else
-// #define ll(StringToLog, ...) // Define as empty to disable logging
-// #endif
-
-
-
-
-// Declare the function
-// void ll(const FString& StringToLog);
-// void ll3(const FString& StringToLog, int SeverityLevel = 0, const FString& Prefix = TEXT("[Info]"));
-// void lll(const std::string& LogMessage);
-
-
 void ClearLogFile();
 void LogAlways(const FString& TextToWrite);
-
-
-void LogMessageInternal(const FString& StringToLog, bool LOG=false, int SeverityLevel = 0, const FString& Prefix = TEXT("[Info]")
-	 // bool file= false
-);
-
+void LogMessageInternal(const FString& StringToLog, bool LOG=false, int SeverityLevel = 0, const FString& Prefix = TEXT("[Info]"));
 
 FVector Jiggle(const FVector& Vec, float Magnitude);
-
 void eeeee();
-
 
 inline FVector RandPointInCircle(float CircleRadius)
 {
@@ -57,46 +26,6 @@ inline FVector RandPointInCircle(float CircleRadius)
 
 	return Point * CircleRadius;
 }
-
-
-///////////////////////////////////////////////////////////////////////////////////////////
-// #include <iostream>
-// #include <chrono>
-//
-// // Function template to measure the execution time of any function
-// template<typename Func, typename... Args>
-// void measureExecutionTime(Func func, Args&&... args) {
-// 	using namespace std::chrono;
-//     
-// 	// Start time
-// 	auto start = high_resolution_clock::now();
-//     
-// 	// Call the function with given arguments
-// 	func(std::forward<Args>(args)...);
-//     
-// 	// End time
-// 	auto end = high_resolution_clock::now();
-//     
-// 	// Calculate and print the elapsed time
-// 	auto duration = duration_cast<microseconds>(end - start).count();
-// 	std::cout << "Execution time: " << duration << " microseconds" << std::endl;
-// }
-
-
-//
-// #include <chrono>
-// #define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
-// #define timeNow() std::chrono::high_resolution_clock::now()
-//
-// typedef std::chrono::high_resolution_clock::time_point TimeVar;
-//
-//
-// template<typename F, typename... Args>
-// double funcTime(F func, Args&&... args){
-// 	TimeVar t1=timeNow();
-// 	func(std::forward<Args>(args)...);
-// 	return duration(timeNow()-t1);
-// }
 
 
 
